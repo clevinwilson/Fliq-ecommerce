2043954ea5c82ae60d814ff2039b207f02f69be0
Clevin M.Wilson <clevinmwilson@gmail.com>
2023-01-20 10:11:56 +0530

    verify ajax request function added

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,11 +1,13 @@
 const verifyLogin = (req, res, next) => {
+    console.log(req.session);
     if (req.session.loggedIn) {
         next()
     } else {
         let err = new Error("You are not authenticated");
         res.setHeader("WWW-Authenticate", "Basic");
         err.status = 401;
-        res.redirect('/login')
+        res.redirect('/login');
+        res.json({status:false})
     }
 }
 
